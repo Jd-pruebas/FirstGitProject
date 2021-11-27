51a4b507cbe0353474658967105dce9180f3dbfd
Juan David Díaz <juandiazr95@gmail.com>
2021-11-27 11:42:40 -0500

    Mi primer proyecto python en Git

---
+++ calculator.py
@@ -0,0 +1,23 @@
+def run():
+    print("\nEsta es una mini calculadora\nA continuación ingresa dos números para hacer operaciones\n")
+    a = int(input("Digita el primer número: "))
+    b = int(input("Digita el segundo número: "))
+    opcion = 0
+    menu = """
+    
+    Opciones:
+
+1 - suma            (número 1 + número 2)
+2 - resta           (número 1 - número 2)
+3 - multiplicación  (número 1 x número 2)
+4 - división        (número 1 ÷ número 2)
+
+Elige una opcion: """
+
+    while opcion < 1 or opcion > 4:
+        opcion = int(input(menu))
+        if opcion < 1 or opcion > 4:
+            menu = "¡Opción incorrecta! Selecciona de nuevo: "
+
+if __name__ == '__main__':
+    run()
