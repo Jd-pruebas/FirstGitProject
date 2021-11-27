ff653c916ece3b9630871228b4dacc45670c3f13
Juan David Díaz <juandiazr95@gmail.com>
2021-11-27 11:56:53 -0500

    Agrego las operaciones de multiplicación y división

--- calculator.py
+++ calculator.py
@@ -6,6 +6,17 @@ def subtraction(num1, num2):
     print("La resta es: " + str(num1 - num2))
 
 
+def multiplication(num1, num2):
+    print("La multiplicación es: " + str(num1 * num2))
+
+
+def divison(num1, num2):
+    if num2 == 0:
+        print("No es posible dividir entre '0'")
+    else:
+        print("La división es: " + str(num1 / num2))
+
+
 def run():
     print("\nEsta es una mini calculadora\nA continuación ingresa dos números para hacer operaciones\n")
     a = int(input("Digita el primer número: "))
